0c1bc1bfa179bc562677b04290888e19b1a0d945
Amelia Aboujawdah <amelia.abou6363@gmail.com>
2022-03-11 18:46:36 -0800

    follow-along from lecture march 11 2022

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,17 @@
+<!-- Footer Block -->
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="container text-center col-md-4 mb-0 text-muted">© 2022 Aboujawdah, Inc.</p>
+
+    <!-- <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Constellations</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Favorites</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul> -->
+</footer>
+
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,27 @@
+<!-- View stored in resources/views/greeting.blade.php -->
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
+</head>
+
+<body>
+    <!-- Header Block -->
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+            <span class="fs-4">Amelia's Space Website</span>
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Constellations</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Favorites</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        </ul>
+    </header>
